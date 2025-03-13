7180f784846253ab84c9a955216d3bd0ebc0aae5
hallonfras <isaacwesterman@gmail.com>
2025-03-13 20:20:41 +0100

    more theming

--- home/theming/schemes.nix
+++ home/theming/schemes.nix
@@ -3,29 +3,7 @@
     slug = "melange-light";
     scheme = "Melange light, (taken from savq/melange-nvim)";
     author = "savq";
-    base00 = "e9e1db";
-    base01 = "c77b8b";
-    base02 = "6e9b72";
-    base03 = "bc5c00";
-    base04 = "7892bd";
-    base05 = "be79bb";
-    base06 = "739797";
-    base07 = "7d6658";
-    base08 = "a98a78";
-    base09 = "bf0021";
-    base0A = "3a684a";
-    base0B = "a06d00";
-    base0C = "465aa4";
-    base0D = "904180";
-    base0E = "3d6568";
-    base0F = "54433a";
-  };
-
-  _module.args.melange-dark = {
-    slug = "melange-dark";
-    scheme = "Melange dark, (taken from savq/melange-nvim)";
-    author = "savq";
-    base00 = "#e9e1db";
+    base00 = "#f1f1f1";
     base01 = "#c77b8b";
     base02 = "#6e9b72";
     base03 = "#bc5c00";
@@ -41,5 +19,27 @@
     base0D = "#904180";
     base0E = "#3d6568";
     base0F = "#54433a";
+  };
+
+  _module.args.melange-dark = {
+    slug = "melange-dark";
+    scheme = "Melange dark, (taken from savq/melange-nvim)";
+    author = "savq";
+    base00 = "292522";
+    base01 = "bd8183";
+    base02 = "78997a";
+    base03 = "e49b5d";
+    base04 = "7f91b2";
+    base05 = "b380b0";
+    base06 = "7b9695";
+    base07 = "c1a78e";
+    base08 = "867462";
+    base09 = "d47766";
+    base0A = "85b695";
+    base0B = "ebc06d";
+    base0C = "a3a9ce";
+    base0D = "cf9bc2";
+    base0E = "89b3b6";
+    base0F = "ece1d7";
   };
 }
